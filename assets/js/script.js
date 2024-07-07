21266f6c45c7589c776c73afa835429fcc9d2213
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-07-07 23:49:31 +1000

    Revamped README and cleaned up comments in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
 function generateTaskId() {
     if (!nextId) {
         nextId = 1;
@@ -14,7 +13,6 @@ function generateTaskId() {
     return nextId;
 }
 
-// Todo: create a function to create a task card
 function createTaskCard(task) {
     const taskCard = $("<div>").addClass("card text-center task-card mb-3 z-index-1").attr("id", task.id);
     const taskTitle = $("<h3>").addClass("card-header mb-3").text(task.title);
@@ -29,11 +27,14 @@ function createTaskCard(task) {
 
 }
 
+function reformatDate(date) {
+    return dayjs(date).format("DD/MM/YYYY")
+}
+
 function updateTaskCardColour(taskCard, date) {
     const today = dayjs();
     const dueDate = dayjs(date);
     const daysUntilDue = dueDate.diff(today, "day");
-    console.log(daysUntilDue);
     if (daysUntilDue <= 0) {
         taskCard.addClass("bg-danger");
     } else if (daysUntilDue <= 3) {
@@ -41,27 +42,23 @@ function updateTaskCardColour(taskCard, date) {
     }
 }
 
-// Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
     $("#todo-cards, #in-progress-cards, #done-cards").empty();
     if (taskList) {
         taskList.forEach(task => {
             let taskCard = createTaskCard(task);
             updateTaskCardColour(taskCard, task.dueDate);
-            taskCard.draggable(
-                {
-                    revert: "invalid",
-                    cursor: "move",
-                    opacity: 0.9,
-                    zIndex: 3
-                }
-            );
-            
+            taskCard.draggable({
+                revert: "invalid",
+                cursor: "move",
+                opacity: 0.9,
+                zIndex: 3
+                
+            });  
         });
     }
 }
 
-// Todo: create a function to handle adding a new task
 function handleAddTask(event){
     event.preventDefault();
     const task = {
@@ -84,29 +81,19 @@ function handleAddTask(event){
 
 }
 
-function reformatDate(date) {
-    return dayjs(date).format("DD/MM/YYYY")
-}
 
-// Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
     const taskId = $(this).closest(".task-card").attr("id");
-    console.log(taskId);
     taskList = taskList.filter(task => task.id !== parseInt(taskId));
-    console.log(taskList);
     localStorage.setItem("tasks", JSON.stringify(taskList));
     renderTaskList();
 
 }
 
-// Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
     event.preventDefault();
-    console.log(ui);
     const taskId = ui.draggable.attr("id");
-    console.log(taskId);
     const status = $(this);
-    console.log(this.dataset.status);
     const task = taskList.find(task => task.id === parseInt(taskId)); 
     task.status = this.dataset.status;
     localStorage.setItem("tasks", JSON.stringify(taskList));
@@ -114,7 +101,6 @@ function handleDrop(event, ui) {
 
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     renderTaskList();
     $("#add-task").on("click", handleAddTask);
